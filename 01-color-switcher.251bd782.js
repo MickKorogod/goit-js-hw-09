ee7cf404b53fd24c4d007e43c09826597b61053f
MickKorogod <99369016+MickKorogod@users.noreply.github.com>
2022-08-04 21:58:43 +0000

    Deploying to gh-pages from @ MickKorogod/goit-js-hw-09@24314116b40f1a738f1a4d0fa6b0f69efee69990 🚀

---
+++ 01-color-switcher.251bd782.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(){r=setInterval((function(){return n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(r),t.setAttribute("disabled",!1)}))}();
+//# sourceMappingURL=01-color-switcher.251bd782.js.map
---
+++ 01-color-switcher.3edf254d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>r.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(a),t.setAttribute("disabled",!1)}));
+//# sourceMappingURL=01-color-switcher.3edf254d.js.map
